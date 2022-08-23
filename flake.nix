4d31987b35e1784e78a874bd0546c12fb4bf12f1
Erik Karsten <erik.karsten@eficode.com>
2022-08-23 13:28:09 +0300

    initial with flake env

---
+++ flake.nix
@@ -0,0 +1,60 @@
+{
+  description = "Bitbucket merging assistant";
+
+  inputs = {
+    flake-utils.url = "github:numtide/flake-utils";
+    nixpkgs-unstable.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-compat = {
+      url = "github:edolstra/flake-compat";
+      flake = false;
+    };
+  };
+
+  outputs = { self, nixpkgs-unstable, flake-utils, ...}:
+    let
+      version = "0.0.1";
+      supportedSystems = [ "x86_64-linux" "x86_64-darwin" "aarch64-linux" "aarch64-darwin" ];
+      forAllSystems = nixpkgs-unstable.lib.genAttrs supportedSystems;
+
+      nixpkgsFor = forAllSystems (system: import nixpkgs-unstable { inherit system; });
+
+    in {
+      packages = forAllSystems (system:
+        let pkgs = nixpkgsFor.${system};
+        in {
+          toke-rs = pkgs.rustPlatform.buildRustPackage {
+            pname = "toke-rs";
+            inherit version;
+            src = ./.;
+            cargoSha256 = "sha256-ZbJc3BTpVZ9vc/PvthXycuuYN6GdqgLQtt8kaUCUCB4=";
+            
+            meta = with pkgs.lib; {
+              description = "Small utility for adding ansible roles from a remote source";
+              license = licenses.mit;
+            };
+          };
+        });
+
+      defaultPackage = forAllSystems (system: self.packages.${system}.toke-rs);
+
+      defaultApp = forAllSystems (system: {
+        type = "app";
+        program = "${self.packages.${system}.toke-rs}/bin/toke";
+      });
+
+      devShell = forAllSystems (system:
+        let
+          system = "x86_64-linux";
+          pkgs = nixpkgsFor.${system};
+        in pkgs.mkShell {
+          packages = with pkgs; [
+            rust-analyzer
+            rustup
+            bashInteractive
+            openssl.dev
+            pkg-config
+            python39Packages.grip
+          ];
+        });
+    };
+}
