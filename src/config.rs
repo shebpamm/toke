73eee56bb9eb8ca4d90d9b5a10cfac9ebcc48873
Erik Karsten <erik.karsten@eficode.com>
2022-08-29 16:38:37 +0300

    use xdg folders by default, implement secret management

--- src/config.rs
+++ src/config.rs
@@ -1,15 +1,37 @@
-use config::Config;
+use config::{Config,ConfigError};
+use xdg;
+use std::fs;
 
-pub fn get_settings() -> Config {
+pub fn get_settings() -> Result<Config, ConfigError> {
+
+    let xdg_dirs = xdg::BaseDirectories::with_prefix("toke").unwrap();
+    let runtime_path = xdg_dirs.place_runtime_file("toke.pid")
+                        .expect("cannot create runtime directory");
+    let config_path = xdg_dirs.place_config_file("config.toml")
+                        .expect("cannot create configuration directory");
+    let stdout_path = xdg_dirs.place_state_file("stdout.log")
+                        .expect("cannot create stdout log directory");
+    let stderr_path = xdg_dirs.place_state_file("stderr.log")
+                        .expect("cannot create stderr log directory");
+
+    // Make sure file exists
+    fs::OpenOptions::new()
+        .write(true)
+        .truncate(false)
+        .create(true)
+        .open(&config_path)
+        .unwrap();
+
     Config::builder()
         // Add in `./toke-config.toml`
-        .add_source(config::File::with_name("./toke-config.toml"))
+        .add_source(config::File::with_name(&config_path.into_os_string().into_string().unwrap()))
         // Add in settings from the environment (with a prefix of APP)
         // Eg.. `APP_DEBUG=1 ./target/app` would set the `debug` key
         .add_source(config::Environment::with_prefix("TOKE"))
 
         // Defaults
-        .set_default("pidfile", "/var/run/user/$UID/toke.pid").unwrap()
+        .set_default("pidfile", runtime_path.into_os_string().into_string().unwrap())?
+        .set_default("stdout",  stdout_path .into_os_string().into_string().unwrap())?
+        .set_default("stderr",  stderr_path .into_os_string().into_string().unwrap())?
         .build()
-        .unwrap()
 }
